a0a50fb78ebae87b127595e543be34b3ce2df5a8
mihle2025 <mihletshona3@gmail.com>
2025-10-31 15:44:11 +0200

    send

---
+++ conditional.js
@@ -0,0 +1,14 @@
+
+let score = 85;
+let passingGrade = 60;
+
+
+if (score >= passingGrade) {
+  if (score === 100) {
+    console.log("Perfect score! Excellent work!");
+  } else {
+    console.log("Passed");
+  }
+} else {
+  console.log("Failed");
+}
