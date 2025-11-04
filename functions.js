03e729383200811a21798ad17d4817f3e268ad38
mihle2025 <mihletshona3@gmail.com>
2025-11-04 10:19:33 +0200

    first commit

---
+++ functions.js
@@ -0,0 +1,14 @@
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+let greeting = greet("Mihle");
+let adding = add(67, 5);
+
+console.log(greeting);
+console.log(adding);
+
